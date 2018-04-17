7fd4154747eb917275ff00693d8092cdd09d54a9
Adamlin0708 <kuso12610@gmail.com>
2018-04-17 16:43:34 +0800

    #00008: 使用者可以CRUD文章 -- PostController新增new, create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,28 @@ class PostsController < ApplicationController
     @posts = Post.all    
   end
 
+  def new
+    @post = Post.new
+    @categories = Category.all
+  end
+
+  def create
+    @post = Post.new(posts_params)
+    @post.user_id = current_user.id
+    
+    if @post.save
+      flash[:notice] = "新增成功"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "新增失敗！"
+      render :new
+    end
+  end
+
+
+  #-------------private--------------#
+
+  def posts_params
+    params.require(:post).permit(:title, :content, :image, :categories => [])
   end
 end
