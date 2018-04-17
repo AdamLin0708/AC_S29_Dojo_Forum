cad7cee8ac86c2676cd8f902b9574cdb772b9534
Adamlin0708 <kuso12610@gmail.com>
2018-04-17 22:08:42 +0800

    #00011: 後台使用者列表 -- 新增Admin::UserController的update method

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,17 @@ class Admin::UsersController < Admin::BaseController
   def index
     @users = User.all
   end
+
+  def update
+
+    @user = User.find(params[:id])
+    @user.role = params[:role]
+    @user.save!
+
+    flash[:notice] = "編輯成功"    
+    redirect_back(fallback_location: root_path)    
+    
+  end
+
+
 end
