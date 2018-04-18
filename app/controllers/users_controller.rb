8f9c1471960929eab294e88c6e353722dc21fb89
Adamlin0708 <kuso12610@gmail.com>
2018-04-18 14:37:05 +0800

    #00012: 使用者可修改個人資料 -- 新增UsersController，以及profile, edit, update method

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+
+  before_action :set_user, only: [:profile, :edit, :update]
+
+  def profile
+  end
+
+  def edit    
+  end
+
+
+  def update
+    if @user.update(users_params)
+      flash[:notice] = "編輯成功"      
+    else
+      flash.now[:alert] = "編輯失敗"      
+    end
+
+    redirect_to profile_user_path(@user.id)
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:username, :introduction, :avatar )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
