3d071490a3fcc7a1ec5d94c8dd854e633a59a094
Adamlin0708 <kuso12610@gmail.com>
2018-04-20 18:06:41 +0800

    #00014: 分類若有文章，則管理員無法刪除 -- 新增PostCategory Model，並設定和Post, Category之間的關係

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ApplicationRecord
   validates_presence_of :name
   validates_uniqueness_of :name       
   
+  has_many :post_categories, dependent: :destroy
+  has_many :setted_posts, through: :post_categories, source: :post
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,10 @@ class Post < ApplicationRecord
 
   mount_uploader :image, PostImageUploader
 
-  validates_presence_of :title, :content
-  serialize :categories, Array
+  validates_presence_of :title
   belongs_to :user
   has_many :replies, dependent: :destroy
+  has_many :post_categories, dependent: :destroy
+  has_many :setted_categories, through: :post_categories, source: :category
   
 end
