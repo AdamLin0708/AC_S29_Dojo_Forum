22fd60516a7cccf4d46c43f6c84571b941cac4bc
Adamlin0708 <kuso12610@gmail.com>
2018-04-20 19:04:28 +0800

    #00015: 使用者可以編輯、刪除自己的回覆 -- RepliesController新增edit, update, destroy method

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,5 +1,7 @@
 class RepliesController < ApplicationController
 
+  before_action :set_reply, only: [:edit, :update, :destroy]
+
   def create
     @reply = Reply.new(reply_params)
     @reply.user_id = current_user.id
@@ -13,10 +15,29 @@ class RepliesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @reply.update(params.require(:reply).permit(:comment))
+    flash[:notice] = "成功更新您的回覆！"
+    redirect_to post_path(@reply.post_id)
+  end
+
+  def destroy
+    @reply.destroy
+    flash[:notice] = "成功刪除您的回覆！"
+    redirect_back(fallback_location: root_path)    
+  end
+
   private
 
   def reply_params
     params.require(:reply).permit(:comment, :post_id)
+  end
+
+  def set_reply
+    @reply = Reply.find(params[:id])
   end
 
 end
