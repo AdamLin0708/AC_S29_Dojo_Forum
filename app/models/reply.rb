265aef857c067e46f0407b2766c1d3c9404461e4
Adamlin0708 <kuso12610@gmail.com>
2018-05-09 23:54:33 +0800

    #00022: 全站快訊 -- user table新增replies_count column

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,6 @@
 class Reply < ApplicationRecord
 
   belongs_to :post, counter_cache: true, optional: true
-  belongs_to :user, optional: true
+  belongs_to :user, counter_cache: true, optional: true
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180502150017) do
+ActiveRecord::Schema.define(version: 20180509154820) do
 
   create_table "categories", force: :cascade do |t|
     t.string "code_abbr"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180502150017) do
     t.string "username"
     t.text "introduction"
     t.string "avatar"
+    t.integer "replies_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
